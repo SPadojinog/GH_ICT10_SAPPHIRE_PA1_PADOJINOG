f2093471ccc0fb7f88e519ed22fc3b235789e9f9
SPADO <padojinog.sf@obmontessori.edu.ph>
2026-01-12 11:23:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Temperature Converter with Fever Check
+from pyscript import document
+
+def convert_temperature(e):
+    result_element = document.getElementById('output')
+    result_element.innerHTML = ""
+
+    fahrenheit = float(document.getElementById('fahrenheit').value)
+
+    # Convertion
+    celsius = (fahrenheit - 32) * 5 / 9
+
+    # Checking
+    if celsius >= 37.8:
+        status = "Fever detected"
+    else:
+        status = "No fever"
+
+    # Display result
+    result = f"""
+    🌡️ <b>Temperature in Celsius:</b> {celsius:.2f} °C<br>
+    🩺 <b>Status:</b> {status}
+    """
+
+    result_element.innerHTML = result
